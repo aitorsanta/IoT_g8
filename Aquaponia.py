4c18b5b0b90b5892e9590cc2ba05ea7e81ec5beb
Xabiroi <xsarri9@opendeusto.es>
2019-01-13 19:43:27 +0100

    Cambio de excepciones y bucles

--- Aquaponia.py
+++ Aquaponia.py
@@ -330,18 +330,18 @@ def gas():
     GPIO.setup(4, GPIO.IN) #Indicamos que el pin 4 sera de entrada
     GPIO.setup(7, GPIO.OUT) #Indicamos que el pin 7 sera de salida
     GPIO.output(7,True) #Indicamos que el pin 7 esta LOW (sin senal)
-    try:
-        if GPIO.input(4): #Si detectamos que el sensor se ha activado por la presencia de vapores
-            print("Humo detectado") #Sacamos por pantalla HUMO DETECTADO
-            GPIO.output(7, False) #Enviamos la senal de activacion al buzzer pin 7 HIGH
-            time.sleep(5) #La senal (pitido) dura 5 segundos
-            GPIO.output(7,True) #Cerramos la senal poniendo el pin 7 en LOW y el buzzer se calla.
-        # Seguimos a la espera de otra senal por parte del sensor MQ-135
-        else:
-            print("No hay gases toxicos")
-    except KeyboardInterrupt:
-        print("El usuario ha forzado la detencion del script")
-        GPIO.cleanup()
+#     try:
+    if GPIO.input(4): #Si detectamos que el sensor se ha activado por la presencia de vapores
+        print("Humo detectado") #Sacamos por pantalla HUMO DETECTADO
+        GPIO.output(7, False) #Enviamos la senal de activacion al buzzer pin 7 HIGH
+        time.sleep(5) #La senal (pitido) dura 5 segundos
+        GPIO.output(7,True) #Cerramos la senal poniendo el pin 7 en LOW y el buzzer se calla.
+    # Seguimos a la espera de otra senal por parte del sensor MQ-135
+    else:
+        print("No hay gases toxicos")
+#     except KeyboardInterrupt:
+#         print("El usuario ha forzado la detencion del script")
+#         GPIO.cleanup()
     #Con keyboardinterrupt detectamos si el usuario pulsa CONTROL + C y si es asi 
     #cerramos el script y "limpiamos" los pines GPIO con GPIO.cleanup()
 
@@ -362,8 +362,8 @@ def humedad():
     GPIO.add_event_callback(channel, callback)  # assign function to GPIO PIN, Run function on change
      
     # infinite loop
-    while True:
-        time.sleep(1)
+#     while True:
+#         time.sleep(1)
 
 
 
